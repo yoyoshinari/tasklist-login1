776127acd06b7be6940c52d532493200e55e7cad
yoyoshinari <yoshinari331751@gmail.com>
2018-11-16 08:23:05 +0000

    before final

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,5 @@
 class ToppagesController < ApplicationController
   def index
-    @tasks=Task.all
     if logged_in?
       @task = current_user.tasks.build  # form_for 用
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
