0c24da850bd29349d2f0790e99316707ec86ede7
yoyoshinari <yoshinari331751@gmail.com>
2018-11-30 04:35:19 +0000

    30nov

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
     @tasks = current_user.tasks.page(params[:page])
@@ -51,5 +52,11 @@ class TasksController < ApplicationController
   end
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+    redirect_to root_url
+    end
   end
 end
